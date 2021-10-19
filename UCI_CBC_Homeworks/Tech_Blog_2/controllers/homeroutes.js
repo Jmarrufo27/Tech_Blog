47b3c2ad82b555a51a43da931f961945c58dac7c
Jose Marrufo <jose.marrufo@rocketmail.com>
2021-10-19 08:00:54 -0700

    Npm installed

--- UCI_CBC_Homeworks/Tech_Blog_2/controllers/homeroutes.js
+++ UCI_CBC_Homeworks/Tech_Blog_2/controllers/homeroutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogRoutes');
+const {Blog, User} = require('../models');
+const withAuth = require('../utils/auth')
 
 router.use('/users', userRoutes);
 router.use('/projects', blogRoutes);
